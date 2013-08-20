a2cce04ce3e2bb8eb8874c7d474022115d0c24f6
kasperific <sharon@sharon-kasper.com>
2013-08-20 12:58:54 -0400

    Create footer.php
    Footer navigation markup.

---
+++ footer.php
@@ -0,0 +1,89 @@
+<footer class="container_12">
+    <div class="grid_2">
+        
+      <h3>New York</h3>
+
+        <ul>
+            <li class="departure"><a class="last" href="/morocco/departure.php">Departure</a>
+            </li>
+        </ul>
+    </div>
+    <div class="grid_2">
+        
+      <h3>Tangier</h3>
+
+        <ul>
+            <li class="arrival"><a href="/morocco/arrival.php">Arrival</a>
+            </li>
+            <li class="tangier"><a class="last" href="/morocco/tangier.php">Aging Hippies</a>
+            </li>
+        </ul>
+    </div>
+    <div class="grid_2">
+        
+      <h3>Fes</h3>
+
+        <ul>
+            <li class="fes"><a href="/morocco/fes.php">Lift into Town</a>
+            </li>
+            <li class="fes2"><a href="/morocco/fes2.php">Meeting Our Hosts</a>
+            </li>
+            <li class="fes3"><a class="last" href="/morocco/fes3.php">Getting Lost, Again</a>
+            </li>
+        </ul>
+    </div>
+    <div class="grid_2">
+        
+      <h3>The Desert Tour</h3>
+
+        <ul>
+            <li class="desert"><a href="/morocco/desert.php">No Time for Love</a>
+            </li>
+            <li class="desert2"><a href="/morocco/desert2.php">Freaking Out</a>
+            </li>
+            <li class="desert3"><a href="/morocco/desert3.php">Camel Trek</a>
+            </li>
+            <li class="desert4"><a href="/morocco/desert4.php">Tripping in the Sahara</a>
+            </li>
+            <li class="desert5"><a href="/morocco/desert5.php">Jallah</a>
+            </li>
+            <li class="desert6"><a href="/morocco/desert6.php">Incident in the Valley of Roses</a>
+            </li>
+            <li><a class="nolink">That Place Where Gladiator was Filmed</a>
+            </li>
+            <li><a class="nolink last">Tour Ends</a>
+        </ul>
+    </div>
+    <div class="grid_2">
+        
+      <h3>Marrakech</h3>
+
+        <ul>
+            <li><a class="nolink">Hammam Time</a>
+            </li>
+            <li><a class="nolink">Henna Tattoo</a>
+            </li>
+            <li><a class="nolink">Dinner in the Djemaa el-Fna</a>
+            </li>
+            <li><a class="nolink last">Getting Lost, Again</a> 
+        </ul>
+    </div>
+    <div class="grid_2">
+        
+      <h3>Madrid</h3>
+
+        <ul>
+            <li><a class="nolink">Left my Keys in Marrakesh</a>
+            </li>
+            <li><a class="nolink">The Tourist Office</a>
+            </li>
+            <li><a class="nolink">Feliz Navidad</a>
+            </li>
+            <li><a class="nolink">Our Modest Lodging</a>
+            </li>
+            <li><a class="nolink">Hunt for Food</a>
+            </li>
+            <li><a class="nolink last">No Sleep 'til Brooklyn (or Queens)</a>
+        </ul>
+    </div>
+</footer>
